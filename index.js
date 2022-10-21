7cf44544f38cb2c1951b759ddbab420a5bf34cfc
Mason <davismasont@gmail.com>
2022-10-21 00:39:12 -0700

    added object constructs for classes in there relative files

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const EmployConst = require('./lib/Employee.js')
+const EngineerConst = require('./lib/Engineer.js')
+const ManagerConst = require('./lib/Manager.js')
+const InternConst = require('./lib/Intern.js')
+const employee = new EmployConst('kyle','45','@kyle')
+const engine = new EngineerConst('kyle','45','@kyle','717')
+const intern = new InternConst('kyle','14','@kyle','school place')
+console.log(intern.getRole())
+
+
 function sum(a, b) {
     return a + b;
   }
